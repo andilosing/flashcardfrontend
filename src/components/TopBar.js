6a51a8418f35f107ffeaf3ceea82cc12ea841619
andilosing <andreas.losing@gmx.de>
2024-03-14 10:25:30 +0100

    fixed bug streak = 0

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,19 +7,19 @@ function TopBar({ isActive, onNavBarClick }) {
   const loggedInUser = useSelector((state) => state.users.user);
 
   const getInitials = (username) => {
-    return username ? username.substring(0, 2).toUpperCase() : '';
-  }
+    return username ? username.substring(0, 2).toUpperCase() : "";
+  };
 
   const getLearningTimeColor = (minutes) => {
     if (minutes < 30) return "salmon";
     if (minutes > 60) return "lightgreen";
-    return "khaki"; 
+    return "khaki";
   };
 
   const getStreakColor = (days) => {
     if (days < 15) return "salmon";
     if (days > 30) return "khaki";
-    return "lightgreen"; 
+    return "lightgreen";
   };
 
   return (
@@ -28,22 +28,34 @@ function TopBar({ isActive, onNavBarClick }) {
         <FaBars size="30px" />
       </div>
       <div className="topbar-info">
-        {loggedInUser && loggedInUser.averageLearningTimePerDayMin &&
-          <div className="learning-info">
-            <FaClock size="25px" color={getLearningTimeColor(loggedInUser.averageLearningTimePerDayMin)} />
-            <span>{loggedInUser.averageLearningTimePerDayMin} min</span>
-          </div>}
-        {loggedInUser && loggedInUser.learnignStreakInDays &&
+        {loggedInUser &&
+          loggedInUser.averageLearningTimePerDayMin !== undefined && (
+            <div className="learning-info">
+              <FaClock
+                size="25px"
+                color={getLearningTimeColor(
+                  loggedInUser.averageLearningTimePerDayMin
+                )}
+              />
+              <span>{loggedInUser.averageLearningTimePerDayMin} min</span>
+            </div>
+          )}
+
+        {loggedInUser && loggedInUser.learnignStreakInDays !== undefined && (
           <div className="streak-info">
-            <FaFire size="25px" color={getStreakColor(loggedInUser.learnignStreakInDays)} />
-            <span>{loggedInUser.learnignStreakInDays}</span>
-          </div>}
+            <FaFire
+              size="25px"
+              color={getStreakColor(loggedInUser.learnignStreakInDays)}
+            />
+            <span>{loggedInUser.learnignStreakInDays} days</span>
+          </div>
+        )}
       </div>
-      {loggedInUser && loggedInUser.username && 
-        <div className="user">{getInitials(loggedInUser.username)}</div>}
+      {loggedInUser && loggedInUser.username && (
+        <div className="user">{getInitials(loggedInUser.username)}</div>
+      )}
     </section>
   );
-
 }
 
 export default TopBar;
